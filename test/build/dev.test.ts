b660e1d3a1e4d79d1dd487656ecdd6d61165dc33
Fatih Mar <mfatihmar@gmail.com>
2024-11-04 22:19:34 +0000

    custom build and instructions

--- test/build/dev.test.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-
-describe('dev build', () => {
-    test('file is not empty', () => {
-        expect(fs.readFileSync('dist/maplibre-gl-dev.js', 'utf8').length).toBeGreaterThan(0);
-    });
-});
