29185ba148d98398a41d8704a539f6f2ce130382
Fatih Mar <mfatihmar@gmail.com>
2024-12-18 21:35:23 +0300

    custom package build

--- test/build/dev.test.ts
+++
@@ -1,8 +0,0 @@
-import {describe, test, expect} from 'vitest';
-import fs from 'fs';
-
-describe('dev build', () => {
-    test('file is not empty', () => {
-        expect(fs.readFileSync('dist/maplibre-gl-dev.js', 'utf8').length).toBeGreaterThan(0);
-    });
-});
